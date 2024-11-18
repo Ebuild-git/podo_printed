497539f9bf1ce49ac5be66193bf2b9272bc381f4
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2024-11-18 14:25:33 +0100

    update

--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -273,7 +273,8 @@ $produit = DB::table('produits')->get();
 
 
     </main>
-
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+  
     <script>
 	    var botmanWidget = {
 	        aboutText: 'ssdsd',
