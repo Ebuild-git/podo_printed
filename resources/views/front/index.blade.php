9097c3dfe7d72887858711e18ba17a28e3403838
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2024-11-18 14:46:14 +0100

    update

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -846,7 +846,7 @@
         };
     </script> --}}
   
-    <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+     <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script> 
       
 </main>
 
