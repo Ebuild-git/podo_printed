bbd8c50b3db0ec613c266c8fe546aac5abff7937
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2024-11-24 20:05:14 +0100

    update

--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -293,7 +293,7 @@
 
     </main>
 
-
+{{-- 
     <script>
         var botmanWidget = {
         
@@ -312,8 +312,22 @@
     </script>
 
     <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
-
-
+ --}}
+<!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6743708b2480f5b4f5a35bc4/1idfmv918';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+    <!--End of Tawk.to Script-->
     <!-- Footer Start -->
     <footer class="main-footer">
         <div class="container">
