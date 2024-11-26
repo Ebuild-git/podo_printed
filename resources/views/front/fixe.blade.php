c3b4ecf235fd3fa874a8574e118fd962116bb05d
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2024-11-26 21:09:23 +0100

    update

--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -315,6 +315,8 @@
  --}}
 <!--Start of Tawk.to Script-->
 <!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
     (function(){
@@ -326,6 +328,8 @@
     s0.parentNode.insertBefore(s1,s0);
     })();
     </script>
+    <!--End of Tawk.to Script-->
+    <!--End of Tawk.to Script-->
     <!--End of Tawk.to Script-->
     <!--End of Tawk.to Script-->
     <!-- Footer Start -->
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -809,6 +809,9 @@
      <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script> 
 
        --}}
+
+
+       
 </main>
 
 
